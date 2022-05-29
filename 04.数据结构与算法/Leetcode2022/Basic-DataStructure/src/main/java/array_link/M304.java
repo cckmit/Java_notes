e0363a11a204d3e1dd9338afa14ec3049f026d53
Lemonade19 <pxzjl123@icloud.com>
2022-05-29 11:58:27 +0800

    clean

---
+++ 04.数据结构与算法/Leetcode2022/Basic-DataStructure/src/main/java/array_link/M304.java
@@ -0,0 +1,49 @@
+package array_link;
+
+/**
+ * @author cat
+ * @description
+ * @date 2022/5/29 上午11:53
+ */
+
+/**
+计算其子矩形范围内元素的总和，该子矩阵的 左上角 为 (row1, col1) ，右下角 为 (row2, col2) 。
+<p>
+示例 1：
+输入:
+["NumMatrix","sumRegion","sumRegion","sumRegion"]
+[[[[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]],[2,1,4,3],[1,1,2,2],[1,2,2,4]]
+输出:
+[null, 8, 11, 12]
+<p>
+解释:
+NumMatrix numMatrix = new NumMatrix([[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]);
+numMatrix.sumRegion(2, 1, 4, 3); // return 8 (红色矩形框的元素总和)
+numMatrix.sumRegion(1, 1, 2, 2); // return 11 (绿色矩形框的元素总和)
+numMatrix.sumRegion(1, 2, 2, 4); // return 12 (蓝色矩形框的元素总和)
+
+ */
+public class M304 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    static class NumMatrix {
+
+        public NumMatrix(int[][] matrix) {
+
+        }
+
+        public int sumRegion(int row1, int col1, int row2, int col2) {
+            return 0;
+        }
+    }
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
+}
